1ab80e2b23178dccb09f07aa7ba4e9bff69f64c8
JGK <kimjungi9797@gmail.com>
2022-12-17 17:32:24 +0900

    재업

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'public') 
+FRONTEND_DIR = os.path.join(BASE_DIR, 'src')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='ome'),
 ]
